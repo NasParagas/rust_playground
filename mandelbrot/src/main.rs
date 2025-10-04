96e9aecd04aab7545acedca76589f5b039cbb82c
NasParagas <naspara4693@gmail.com>
2025-10-04 21:00:38 +0900

    add

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -19,7 +19,39 @@
 // }
 
 use num::Complex;
+use std::{std::FromStr, str::FromStr};
+
+/**
+ * pub enum Option<T> {
+    /// No value.
+    #[lang = "None"]
+    #[stable(feature = "rust1", since = "1.0.0")]
+    None,
+    /// Some value of type `T`.
+    #[lang = "Some"]
+    #[stable(feature = "rust1", since = "1.0.0")]
+    Some(#[stable(feature = "rust1", since = "1.0.0")] T),
+ */
 
 fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
-    
+    let mut z = Complex { re: 0.0, im: 0.0 };
+    for i in 0..limit {
+        if z.norm_sqr() > 4.0 {
+            return Some(i);  // マンデルブロ集合に含まれている場合
+        }
+        z = z * z + c;
+    }
+    None  // 含まれていると言えない場合
+}
+
+/// 
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+    match s.find(separator) {
+        None => None,
+        Some(index) => {
+            match (T::from_str(&s[..index]), T::from_str(&s[index+1..])) {
+                
+            }
+        }
+    }
 }
