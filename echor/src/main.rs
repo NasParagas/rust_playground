417964b769a8e1e69b7238b200b7deccc761a972
NasParagas <naspara4693@gmail.com>
2025-12-11 23:02:01 +0900

    [update] echor完成

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,10 +1,10 @@
 use clap::{App, Arg};
 
 fn main() -> () {
-    let matches = App::new("echor")
+    let matches = App::new("rust_echo")
         .version("0.1.0")
-        .author("NasParagas <naspara4693@gmail.com>")
-        .about("Rust echor")
+        .author("example <example.ex.co.ex>")
+        .about("echo in rust")
         .arg(
             Arg::with_name("text") // ?
                 .value_name("TEXT") // helpとかで出てきそう
@@ -19,7 +19,21 @@ fn main() -> () {
                 .takes_value(false),
         )
         .get_matches(); // 引数を解析
-    // println!("{:#?}", std::env::args())
+    //
+    // 変数matchesの中身↓
+    // println!("{:#?}", matches);
 
-    println!("{:#?}", matches);
+    let text = matches.values_of_lossy("text").unwrap();
+    // let text: Vec<String> = ... とすることで型を明示して宣言することもできる
+
+    let omit_newline = matches.is_present("omit_newline");
+
+    let mut ending = "\n";
+    if omit_newline {
+        ending = "";
+    }
+    print!("{}{}", text.join(" "), ending);
+    // 上記はまとめて
+    // print!("{}{}", text.join(" "), if omit_newline { "\n" } else { "" });
+    // ともかける(Rustではifは値を返す。elseなしだとユニット型を返す)
 }
